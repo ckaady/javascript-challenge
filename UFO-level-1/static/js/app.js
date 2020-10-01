d68c6f7ab44fbe9acd0d7827dfc77182cf893310
Christina Kaady <christina.kaady@gmail.com>
2020-10-01 11:43:51 -0700

    building out level 1 code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+
+// reference table
+let tbody = d3.select("tbody")
+
+// loop through data and append values
+data.forEach(ufoReport => {
+    let row = tbody.append("tr");
+    Object.values(ufoReport).forEach(value => {
+        let cell = row.append("td");
+        console.log(value);
+    });
+});
+
+
+    
