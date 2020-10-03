d59c2229ec6da534b01af17c38c64db16276de88
Christina Kaady <christina.kaady@gmail.com>
2020-10-02 21:08:09 -0700

    she's workin

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,42 +1,55 @@
-// define variable and import data from data.js
-var tableData = data,
-    button = d3.select("#filter-btn"),
-    reset = d3.select("#reset-btn")
-    form = d3.select("form"),
-    tbody = d3.select("tbody");
+// from data.js
+var tableData = data;
 
-
-console.log(tableData);
-
-// YOUR CODE HERE!
+// get a reference to the table body
+const tbody = d3.select("tbody");
 
 // loop through data and append values
-data.forEach(ufoReport => {
+data.forEach((ufoReport) => {
     let row = tbody.append("tr");
-    Object.values(ufoReport).forEach(value => {
+    Object.values(ufoReport).forEach(value =>{
         let cell = row.append("td");
         cell.text(value);
-        console.log(value);
     });
 });
 
-// event handler
+// select the button
+// select the form - so the "enter" key can be used to submit the selection as well as the button
+let button = d3.select("#filter-btn"),
+    form = d3.select("#form");
+
+// complete the event handler function for the form
 const runEnter = () => {
-    // prevent page refresh when submitting
+
+    // prevent the page from refreshing
     d3.event.preventDefault();
 
     // clear previous table
     tbody.html("");
+    
+    // define input element and value property
+    // get the value property of the input element
 
-    // input element and value property
-    let inputElement = d3.select("#datetime");
-    let inputValue = inputElement.property("#value");
+        let inputElement = d3.select("#datetime"),
+            inputValue = inputElement.property("value");
+  
+        var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+   
+    // check if filter is working and returning correct data 
+        console.log(filteredData);
 
-    // check inputValue
-    console.log(inputValue);
-
-}
-
+    // append filtered values of input element to table
+        filteredData.forEach((entry) => {
+        let row = tbody.append("tr");
+        Object.entries(entry).forEach(function([key, value]) {
+            let cell = row.append("td");
+            cell.text(value);
+        });
+});        
+};
+// create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
 
     
