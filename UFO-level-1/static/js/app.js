54e3cccd6516a22dbe9514b6b2ff5c9d980a0285
Christina Kaady <christina.kaady@gmail.com>
2020-10-02 20:37:20 -0700

    text populating in table and event handler progress

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,5 @@
 // define variable and import data from data.js
-let tableData = data,
+var tableData = data,
     button = d3.select("#filter-btn"),
     reset = d3.select("#reset-btn")
     form = d3.select("form"),
@@ -15,11 +15,28 @@ data.forEach(ufoReport => {
     let row = tbody.append("tr");
     Object.values(ufoReport).forEach(value => {
         let cell = row.append("td");
+        cell.text(value);
         console.log(value);
     });
 });
 
-// 
+// event handler
+const runEnter = () => {
+    // prevent page refresh when submitting
+    d3.event.preventDefault();
+
+    // clear previous table
+    tbody.html("");
+
+    // input element and value property
+    let inputElement = d3.select("#datetime");
+    let inputValue = inputElement.property("#value");
+
+    // check inputValue
+    console.log(inputValue);
+
+}
+
 
 
     
